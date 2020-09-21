6b4e7af91612a7076f83b9bec06ca3acf1b791a3
Jose <jorollu@hotmail.com>
2020-09-21 20:54:28 +0200

    Amchart library loaded and transpiled

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,18 @@
-export default {
+module.exports = {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'sweet-catharsis',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
-
+  script: [
+  ],
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
   ],
@@ -33,5 +34,6 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
-}
+    transpile: ['@amcharts'],
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,13 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        sweet-catharsis with netlify
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <MapChart />
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
